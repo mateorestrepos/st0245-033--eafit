4b8c4e2c5f99ac513398774c89294f0894b4b529
mateorestrepos <30413834+mateorestrepos@users.noreply.github.com>
2017-09-03 21:51:02 -0500

    Add files via upload
    Código de ejercicios en CodingBat

---
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1.java
@@ -0,0 +1,74 @@
+
+public class Recursion1
+
+{
+    
+	//Factorial
+    
+	public int factorial(int n) { 
+        
+		if(n == 1) 										
+            
+			return 1;  									
+        
+	return n * factorial(n - 1);						
+    
+	} 
+    
+	//BunnyEars
+    
+	public int bunnyEars(int bunnies) { 
+        
+		if (bunnies == 0) 								
+            
+			return 0;										
+        
+		return 2 + bunnyEars(bunnies - 1);				
+    
+	}  
+    
+	//Fibonacci
+    
+	public int fibonacci(int n) { 
+        
+		if (n <= 1) 										
+            
+			return n;										
+        
+		return fibonacci(n - 2) + fibonacci(n - 1);  
+    
+	} 
+    
+	//BunnyEars2
+    
+	public int bunnyEars2(int bunnies) { 
+        
+		if (bunnies == 0)                     		
+            
+			return bunnies;                       	
+        
+		if (bunnies % 2 == 1)                    	
+            
+			return 2 + bunnyEars2(bunnies - 1);    	
+        
+	return 3 + bunnyEars2(bunnies - 1);    	
+    
+	} 
+    
+	//Array220
+    
+	public boolean array220(int[] nums, int index) { 
+       
+		if (nums.length < 2 || index == nums.length - 1)
+          
+			return false;										
+       
+		if (nums[index + 1] == nums[index] * 10)			
+          
+			return true;										
+       
+	return array220(nums, index + 1);					
+    
+}
+ 
+}
---
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2.java
@@ -0,0 +1,94 @@
+public class Recursion2
+{
+    
+	//GroupSum
+    
+	public boolean groupSum(int start, int[] nums, int target) { 
+        
+		if (start >= nums.length)                                    
+            
+			return (target == 0);                                    
+        
+		return groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target);                    
+    
+	} 
+    
+	//GroupSum6
+    
+	public boolean groupSum6(int start, int[] nums, int target) { 
+        
+		if (start >= nums.length)                                    
+            
+			return target == 0;                                        
+        
+		if (nums[start] == 6)                                
+            
+			return groupSum6(start + 1, nums, target - nums[start]);
+        
+		return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);              
+    
+	}
+    
+	//GroupNoAdj
+    
+	public boolean groupNoAdj(int start, int[] nums, int target) { 
+        
+		if (start >= nums.length)                                    
+            
+			return target == 0;                                   
+        
+		return groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target);             
+    
+	}
+    
+	//GroupSum5
+    
+	public boolean groupSum5(int start, int [] nums, int target) {
+        
+		if (start >= nums.length)                                  
+            
+			return target == 0;                                     
+        
+		if (nums[start] % 5 == 0){                                
+            
+			if (start < nums.length - 1 && nums[start + 1] == 1)    
+                
+				return groupSum5(start + 2, nums, target - nums[start]);
+            
+			return groupSum5(start + 1, nums, target - nums[start]);
+        
+		}     
+        
+	return groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target);
+    
+	}
+    
+	//GroupSumClump
+    
+	public boolean groupSumClump(int start, int[] nums, int target) { 
+        
+		if (start >= nums.length)                                 
+            
+			return target == 0;                                 
+        
+		int suma = nums[start];                                
+        
+		int count = 1;                                           
+        
+		for (int i = start + 1; i < nums.length; i++){          
+            
+			if (nums[i] == nums[start]){                          
+                
+				suma += nums[i];                                     
+                
+				count++;                                           
+            
+			} 
+        
+		} 
+        
+		return groupSumClump(start + count, nums, target - suma) || groupSumClump(start + count, nums, target); 
+    
+	}
+
+}
