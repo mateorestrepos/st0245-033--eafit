fa62f1f842b0752d58c5e8f073a40e20709566b5
mateorestrepos <30413834+mateorestrepos@users.noreply.github.com>
2017-10-20 19:38:18 -0500

    Update Node.java

--- laboratorios/lab05/codigo/Node.java
+++ laboratorios/lab05/codigo/Node.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Mateo Restrepo Sierra
+ * Nicolas Restrepo Lopez
  */
 package lab5;
 
