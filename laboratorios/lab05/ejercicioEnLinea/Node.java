c735f633cce3059b02f4dc5aff7b54979c90eccb
mateorestrepos <30413834+mateorestrepos@users.noreply.github.com>
2017-10-22 16:40:55 -0500

    Add files via upload

---
+++ laboratorios/lab05/ejercicioEnLinea/Node.java
@@ -0,0 +1,15 @@
+public class Node
+{
+    public Node left;
+    public Node right;
+    public String data;
+    public int intData;
+    public Node (String d)
+    {
+        data = d;
+    }
+    public Node (int d)
+    {
+        intData = d;
+    }
+}
